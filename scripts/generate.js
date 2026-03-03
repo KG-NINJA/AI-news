666c73431a5357065b3ceb098da35a0f887fa581
KG <fuwafuwow@gmail.com>
2026-03-03 15:05:43 +0900

    Update generate.js

--- scripts/generate.js
+++ scripts/generate.js
@@ -52,7 +52,7 @@ ${worldText}
 `
 
     const res = await fetchWithTimeout(
-      "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="+API_KEY,
+      "https://generativelanguage.googleapis.com/v1beta/models/gemini-3-flash-preview:generateContent?key="+API_KEY,
       {
         method:"POST",
         headers:{ "Content-Type":"application/json" },
